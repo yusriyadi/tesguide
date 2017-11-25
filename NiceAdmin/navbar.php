49f831d22bc5eb1a274cfaebf54f9249902312e3
yusriyadi <yusriyadi23@gmail.com>
2017-11-25 17:37:35 +0800

    222

--- NiceAdmin/navbar.php
+++ NiceAdmin/navbar.php
@@ -2,6 +2,21 @@
       <div id="sidebar" class="nav-collapse ">
         <!-- sidebar menu start-->
         <ul class="sidebar-menu">
+
+
+        <li class="active">
+            <a class="" href="index.html">
+                          <i class="icon_house_alt"></i>
+                          <span>Login</span>
+                      </a>
+          </li>   
+           <li class="active">
+            <a class="" href="index.html">
+                          <i class="icon_house_alt"></i>
+                          <span>Logout</span>
+                      </a>
+          </li>       
+
           <li class="active">
             <a class="" href="index.html">
                           <i class="icon_house_alt"></i>
